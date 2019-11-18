6005f4079e6a149b1cdfed622174cc7d34dde3ba
Jonathan Tan <you@example.com>
2019-11-18 13:53:14 -0500

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,45 @@
 // your Bomb code here!
+import React from 'react'
+
+class Bomb extends React.Component{
+    
+    constructor(props) {
+        super() 
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+
+
+    render(){
+
+        // let message 
+
+        // if(this.state.secondsLeft === 0) {
+        //     message = "Boom!"
+        // } else {
+        //     message = `${this.state.secondsLeft} left before I go boom!`
+             
+        // }
+
+       let message =  this.state.secondsLeft === 0? "Boom!" : `${this.state.secondsLeft} seconds left before I go boom!`
+
+        return(
+
+            <p>{message}</p>
+        )
+
+
+
+       
+     
+    
+        
+    }
+    
+
+
+
+}
+
+export default Bomb;
