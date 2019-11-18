8ec91a715045c116d8823dacdc42dc649b591723
Michelle Lee <mlee092@Michelles-MacBook.local>
2019-11-18 09:53:09 -0500

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,24 @@
-// your Bomb code here!
+import React from 'react';
+
+class Bomb extends React.Component {
+    constructor(props){
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+    render(){
+        const message = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`;
+
+        return(
+            <div>{message}</div>
+        )
+    }
+}
+
+export default Bomb;
+
+// {/* <Bomb />
+// ✓ should set the `secondsLeft` to the value of the `initialCount` prop
+// ✓ render the correct text when the timer is not 0
+// ✓ render the correct text when the timer is 0 */}
