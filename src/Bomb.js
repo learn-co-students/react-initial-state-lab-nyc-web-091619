11318c3df3627e1698e848de1ba921a0e080d1d4
H. Turcan Cicek <htcicek@users.noreply.github.com>
2019-11-18 15:40:53 -0500

    met all specs

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,27 @@
 // your Bomb code here!
+import React, { Component } from "react";
+
+export class Bomb extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      secondsLeft: props.initialCount
+    };
+  }
+  timer = setInterval(() => {
+    this.setState({
+      secondsLeft: this.state.secondsLeft - 1
+    });
+  }, 1000);
+  render() {
+    return (
+      <div>
+        {this.state.secondsLeft > 0
+          ? `${this.state.secondsLeft} seconds left before I go boom!`
+          : (clearInterval(this.timer), "Boom!")}
+      </div>
+    );
+  }
+}
+
+export default Bomb;
